4077b12139c35d70b3413b0037836ad034d29b2b
Jonah Latham <jonah.latham21@gmail.com>
2020-01-16 19:05:04 -0500

    code

--- server.js
+++ server.js
@@ -6,9 +6,13 @@ require('dotenv').config()
 const app = express()
 app.use(cors())
 
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static(path.join(__dirname, '/public')));
 
-
+app.get('/*', (req, res) => {
+    res.sendFile('jonahlatham.html', {
+        root: path.join(__dirname, 'public')
+    })
+});
 const port = process.env.PORT || 8090
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
